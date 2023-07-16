60d9b16a71ca7ecdb796f016321f71aa68c3e30f
Lovekiki37332580 <kinyafridah37332580@gmail.com>
2023-07-16 01:04:21 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - use puts function
+ * Return: 0 (should be zero)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}

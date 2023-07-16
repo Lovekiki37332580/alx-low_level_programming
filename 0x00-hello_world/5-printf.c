3409801ea0fc15cb1ce0d38404fb30d10057a0bd
Lovekiki37332580 <kinyafridah37332580@gmail.com>
2023-07-16 01:14:53 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Shows the use of printf
+ * Return: 0 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
